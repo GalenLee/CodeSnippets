a51508a55b0ab9d47096d48efb8dbde5183a8985
Wadzinski, Galen <GWadzinski@jjkeller.com>
2019-10-19 12:26:05 -0500

    Added Rename Files

---
+++ RenameFiles/RenameFiles/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace RenameFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles(@"C:\_projects\AzureDevOps\_Temp", "*_.txt", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                var newFile = file.Substring(0, file.Length - 5);
+                File.Move(file, newFile);
+                Console.WriteLine(newFile);
+            }
+        }
+    }
+}
