8f96aa68ae00332a926fa491d3d7f767732a5379
Wadzinski, Galen <GWadzinski@jjkeller.com>
2019-10-19 12:23:09 -0500

    Setup solution

---
+++ CodeExamples/UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("806943c4-4b71-4e61-83b9-aaee6d0e3fdc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CodeExamples/UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CompositeKeyTests
+    {
+        [TestMethod]
+        public void Test1()
+        { 
+            var a = new CompositeKey<int, int>(1, 2);
+            var b = a.Value1;
+            Assert.AreEqual(1, a.Value1);
+        }
+    }
+}
